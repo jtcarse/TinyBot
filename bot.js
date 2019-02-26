566798a7a601ba8adae98712b1b90cb53e15af7a
John Carse <jtcarse@gmail.com>
2019-02-26 02:58:22 -0600

    Dummy command parsing.

--- bot.js
+++ bot.js
@@ -3,12 +3,16 @@ var client = new Discord.Client();
 var auth = require('./auth.json');
 
 client.on('ready', () => {
-    console.log('Logged in as ' + client.user.tag + '!');
+    console.log('Logged in as ' + client.user.tag + '.');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (message.isMentioned(client.user)) {
+        var msgList = message.content.split(' ').slice(1,);
+        var command = msgList[0];
+        var args = msgList.slice(1,);
+        console.log(command + ':');
+        console.log(args);
     }
 });
 
